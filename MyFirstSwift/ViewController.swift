3ccb2a01284493634a55186b72900dcbbc817766
Jibby <mjiw246@hmail.com>
2016-05-30 15:40:28 +0900

    print 추가

--- MyFirstSwift/ViewController.swift
+++ MyFirstSwift/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let digit = 5;
+        print("touched \(digit)  digit")
     }
 
     override func didReceiveMemoryWarning() {
