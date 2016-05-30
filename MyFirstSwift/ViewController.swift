8a281e5e09efc0bedb440c108b1682bab7dfedbe
Jibby <mjiw246@hmail.com>
2016-05-30 15:46:16 +0900

    test해보는중

--- MyFirstSwift/ViewController.swift
+++ MyFirstSwift/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         let digit = 5;
         print("touched \(digit)  digit")
+        print("touched digit")
+        
     }
 
     override func didReceiveMemoryWarning() {
